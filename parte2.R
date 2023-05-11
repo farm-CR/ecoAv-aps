ca73a6eac67c31df51c5baf3acf61661364f1b4a
Julio-Mugnol <jcmugnol@gmail.com>
2023-05-11 14:37:17 -0300

    Estimacao do hiato do produto

--- parte2.R
+++ parte2.R
@@ -2,12 +2,13 @@ library(readr)
 library(tidyverse)
 library(forecast)
 library(mFilter)
+library(modelsummary)
 
-df <- read_csv("IBC-Br dessazonalizado (2002 = 100).csv")
+ibc <- read_csv("IBC-Br dessazonalizado (2002 = 100).csv")
 ipca <- read_csv("IPCA variacao.csv")
 
-colnames(df) <- c("Data", "Value", "1")
-df <- df %>%
+colnames(ibc) <- c("Data", "Value", "1")
+ibc <- ibc %>%
   select(Data, Value) %>% 
   filter(!is.na(Value))
 
@@ -16,46 +17,33 @@ ipca <- ipca %>%
   select(Data, Value) %>% 
   filter(Data >= 2003.01 & Data <= 2023.02)
 
-df <- ts(df$Value, start = c(2003, 1), frequency = 12)
+ibc <- ts(log(ibc$Value), start = c(2003, 1), frequency = 12)
 ipca <- ts(ipca$Value, start = c(2003, 1), frequency = 12)
 
 
 
-fit <- tslm(df ~ trend)
-summary(fit)
+fit <- tslm(ibc ~ trend)
+modelsummary(list("(2.1)" = fit), stars = c("*" = .1, "**" = .05, "***" = .01), gof_map = c("nobs", "r.squared"))
 acf(residuals(fit))
 pacf(residuals(fit))
 plot(residuals(fit))
 
 data <- data.frame(
-  month = as.numeric(time(df)),
+  month = as.numeric(time(ibc)),
   trend = fit$fitted.values,
   residual = fit$residuals,
-  series = fit$data$df
-)
-ggplot(data, aes(x = month)) +
-  geom_line(aes(y = residual)) +
-  geom_line(aes(y = trend-70)) +
-  geom_line(aes(y = series-70)) +
-  scale_y_continuous(
-    name = "Residuals",
-    sec.axis = sec_axis(~.*2)
-  )
-
-df.hp <- hpfilter(df, freq = 14400)
-# par(mfrow=c(2,1))
-# ts.plot(df, col="blue")
-# lines(df.hp$trend, col="red")
-# ts.plot(df.hp$cycle, col="green4")
-
-data <- data.frame(
-  month = as.numeric(time(df)),
-  series = df,
-  trend = as.numeric(df.hp$trend),
-  residual = df.hp$cycle
-)
-data <- data %>% 
+  series = fit$data$ibc
+) %>% 
   pivot_longer(c(series, trend, residual), names_to = "serie", values_to = "value")
+
+# ggplot(data, aes(x = month)) +
+#   geom_line(aes(y = residual)) +
+#   geom_line(aes(y = trend-4)) +
+#   geom_line(aes(y = series-4)) +
+#   scale_y_continuous(
+#     name = "Residuals",
+#     sec.axis = sec_axis(~.+4)
+#   )
 
 data %>% 
   mutate(residual = serie == "residual") %>% 
@@ -64,12 +52,30 @@ data %>%
   facet_wrap(~residual, nrow = 2, scales = "free")
 
 
+ibc.hp <- hpfilter(ibc, freq = 14400)
+# par(mfrow=c(2,1))
+# ts.plot(df, col="blue")
+# lines(df.hp$trend, col="red")
+# ts.plot(df.hp$cycle, col="green4")
+data <- data.frame(
+  month = as.numeric(time(ibc)),
+  series = ibc,
+  trend = as.numeric(ibc.hp$trend),
+  residual = ibc.hp$cycle
+) %>% 
+  pivot_longer(c(series, trend, residual), names_to = "serie", values_to = "value")
+
+data %>% 
+  mutate(residual = serie == "residual") %>% 
+  ggplot(aes(month, value, color = serie)) +
+  geom_line() +
+  facet_wrap(~residual, nrow = 2, scales = "free")
+
 data <- data.frame(
   month = as.numeric(time(df)),
   hiato = df.hp$cycle,
   ipca = as.numeric(ipca)
 )
-
 data %>% 
   ggplot(aes(x = month)) +
   geom_line(aes(y = hiato), color = "red") +
